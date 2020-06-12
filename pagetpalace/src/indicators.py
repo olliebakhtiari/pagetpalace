9ecb8d23f28e83cb404b37d7889b95f9fc0bc539
Oliver Bakhtiari <oliverb@spirable.com>
2020-06-12 12:56:50 +0100

    modify ssl channel

--- pagetpalace/src/indicators.py
+++ pagetpalace/src/indicators.py
@@ -1,16 +1,27 @@
-# Local.
-from typing import Union
-
 # Third-party.
 import pandas as pd
+import numpy as np
 
 
-def get_ssl_value(df: pd.DataFrame, prices: str = 'mid') -> Union[int, None]:
-    close = df[f'{prices}Close'].apply(pd.to_numeric).iloc[-1]
-    if close > df[f'{prices}High'].apply(pd.to_numeric).mean():
-        return 1
-    if close < df[f'{prices}Low'].apply(pd.to_numeric).mean():
-        return -1
+def ssl_channel(data: pd.DataFrame, prices: str = 'mid', periods: int = 20) -> np.ndarray:
+    close_prices = data[f'{prices}Close'].reset_index(drop=True)
+    high_sma = data[f'{prices}High'].rolling(window=periods).mean()
+    low_sma = data[f'{prices}Low'].rolling(window=periods).mean()
+    hi_lo_vals = np.array([0 for _ in range(len(close_prices))])
+    for i in range(len(high_sma)):
+        if close_prices[i] > high_sma[i]:
+            hi_lo_vals[i] = 1
+        elif close_prices[i] < low_sma[i]:
+            hi_lo_vals[i] = -1
+        else:
+            hi_lo_vals[i] = hi_lo_vals[i - 1]
+
+    return hi_lo_vals
+
+
+def append_ssl_channel(data: pd.DataFrame, periods: int = 20):
+    hlv = ssl_channel(data, periods=periods)
+    data['HighLowValue'] = hlv
 
 
 def append_average_true_range(df: pd.DataFrame, prices: str = 'mid', periods: int = 14):
