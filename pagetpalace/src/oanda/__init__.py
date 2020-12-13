80e86a36ba779196b12fb2a76b88266a37312e06
Ollie Bakhtiari <olliebakhtiari@gmail.com>
2020-12-13 21:11:13 +0000

    import access tokens and account numbers

--- pagetpalace/src/oanda/__init__.py
+++ pagetpalace/src/oanda/__init__.py
@@ -5,6 +5,14 @@ from .pricing import *
 from .stream import *
 from .ssl_multi import SSLMultiTimeFrame
 from .ssl_currency import SSLCurrency
+from .settings import (
+    LIVE_ACCESS_TOKEN,
+    DEMO_ACCESS_TOKEN,
+    DEMO_ACCOUNT_NUMBER,
+    NAS_ACCOUNT_NUMBER,
+    SPX_ACCOUNT_NUMBER,
+    GBP_USD_ACCOUNT_NUMBER,
+)
 
 
 
