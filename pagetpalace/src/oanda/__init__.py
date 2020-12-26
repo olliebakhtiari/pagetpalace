0ec6b3906200926accd3b621d547ae3ff19924c3
Ollie Bakhtiari <olliebakhtiari@gmail.com>
2020-12-26 12:25:58 +0000

    import hammer pin in init

--- pagetpalace/src/oanda/__init__.py
+++ pagetpalace/src/oanda/__init__.py
@@ -2,9 +2,6 @@ from .account import *
 from .instrument import *
 from .orders import *
 from .pricing import *
-from .stream import *
-from .ssl_multi import SSLMultiTimeFrame
-from .ssl_currency import SSLCurrency
 from .settings import (
     LIVE_ACCESS_TOKEN,
     DEMO_ACCESS_TOKEN,
@@ -13,7 +10,7 @@ from .settings import (
     SPX_ACCOUNT_NUMBER,
     GBP_USD_ACCOUNT_NUMBER,
 )
-
-
-
-
+from .ssl_currency import SSLCurrency
+from .ssl_hammer_pin import SSLHammerPin
+from .ssl_multi import SSLMultiTimeFrame
+from .stream import *
