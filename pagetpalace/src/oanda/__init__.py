a16034d829d362c26dc9f10a0663b2ae09e0ce89
Ollie Bakhtiari <olliebakhtiari@gmail.com>
2021-11-01 07:40:40 +0000

    remove unused init imports

--- pagetpalace/src/oanda/__init__.py
+++ pagetpalace/src/oanda/__init__.py
@@ -3,16 +3,3 @@ from .instrument import *
 from .live_trade_monitor import LiveTradeMonitor
 from .orders import Orders
 from .pricing import *
-from .settings import (
-    LIVE_ACCESS_TOKEN,
-    DEMO_ACCESS_TOKEN,
-    DEMO_ACCOUNT_NUMBER,
-    HAMMER_PIN_POOL_ACCOUNT_NUMBER,
-    HEIKIN_ASHI_EWM_1_POOL_ACCOUNT_NUMBER,
-    HEIKIN_ASHI_EWM_2_POOL_ACCOUNT_NUMBER,
-    HPDAILY_ACCOUNT_NUMBER,
-    NAS100_ACCOUNT_NUMBER,
-    SPX500_ACCOUNT_NUMBER,
-    GBP_USD_ACCOUNT_NUMBER,
-    PRIMARY_ACCOUNT_NUMBER,
-)
